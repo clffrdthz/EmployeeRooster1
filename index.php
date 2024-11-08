5166d603ea8ea348665d5186218bd8afd6428fe0
clffrdthz <128756852+clffrdthz@users.noreply.github.com>
2024-11-08 12:06:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
+require_once 'Main.php';
 
-require_once 'app/Person.php';
-require_once 'app/Employee.php';
-require_once 'app/CommissionEmployee.php';
-require_once 'app/HourlyEmployee.php';
-require_once 'app/PieceWorker.php';
-require_once 'app/EmployeeRoster.php';
-require_once 'app/Main.php';
-
-$main = new Main();
-$main->main();
+$entry = new Main();
+$entry->start();
