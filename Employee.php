ea8cc075cb20cfcdb2915b0f804f79d85dcd5eff
clffrdthz <128756852+clffrdthz@users.noreply.github.com>
2024-11-08 12:10:25 +0800

    Update Employee.php

--- Employee.php
+++ Employee.php
@@ -1,23 +1,18 @@
 <?php
+require_once 'Person.php';
 
 abstract class Employee extends Person {
-    private $companyName;
+    private string $companyName;
 
-    public function __construct($companyName) {
-        $this->companyName = $companyName;
-    }
-
-    public function getCompanyName(){
-        return $this->companyName;
-    }
-
-    public function setCompanyName($companyName){
+    public function __construct($name, $address, $age, $companyName) {
+        parent::__construct($name, $address, $age);
         $this->companyName = $companyName;
     }
 
     abstract public function earnings();
 
-    public function __toString(){
-        return $this->companyName;
+    public function __toString(): string {
+        return parent::__toString() . ", Company: {$this->companyName}";
     }
-}
+} 
+
