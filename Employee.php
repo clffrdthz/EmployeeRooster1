b7d55ac75fb4badca8ccb100a4a72de7c6468c42
clffrdthz <128756852+clffrdthz@users.noreply.github.com>
2024-11-09 22:42:39 +0800

    Update Employee.php

--- Employee.php
+++ Employee.php
@@ -1,18 +1,26 @@
 <?php
-require_once 'Person.php';
 
-abstract class Employee extends Person {
-    private string $companyName;
+abstract class Employee {
+    protected string $name;
+    protected string $address;
+    protected int $age;
+    protected string $companyName;
+    
 
-    public function __construct($name, $address, $age, $companyName) {
-        parent::__construct($name, $address, $age);
+    public function __construct(string $name, string $address, int $age, string $companyName) {
+        $this->name = $name;
+        $this->address = $address;
+        $this->age = $age;
         $this->companyName = $companyName;
     }
 
-    abstract public function earnings();
 
-    public function __toString(): string {
-        return parent::__toString() . ", Company: {$this->companyName}";
+
+    abstract public function calculatePay(): float;
+
+    public function getDetails(): string {
+        return "\nName     : {$this->name}\nAddress  : {$this->address}\nAge      : {$this->age}\nCompany  : {$this->companyName}";
     }
-} 
+}
 
+?>
